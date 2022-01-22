a29939032455873e862f848365d12cdadff20315
TriveniprasadGupta <iam.trivenigupta@gmail.com>
2022-01-22 13:31:29 +0530

    Page merged and linked

---
+++ components/navbar.js
@@ -0,0 +1,243 @@
+function navbar(){
+    return `
+    <div id="nav">
+        <a href="./homefinal.html"><img src="https://bit.ly/3nBvG0c" alt="" /></a>
+        <div id="srch">
+            <select>
+                <option value="">Products</option>
+                <option value="">Suppliers</option>
+            </select>
+            <input id="search" type="search" placeholder="What are you looking for. . . " />
+            <!-- <i class="fas fa-camera fa-2x "></i> -->
+            <button>Search</button>
+        </div>
+        <div id="icns">
+            <a href="#">
+                <div id="asign">
+                    <div style="color: black;" id="onblock"><br><br>
+                        <h3>Welcome back!</h3>
+                        <button onclick = "window.location.href = './signin.html'">Sign In</button><br><br>
+                        <button onclick = "window.location.href = './signUp.html'">Join Free</button><br><br>
+                        _______Continue With_______ <br><br>
+                      <i class="fab fa-facebook-square fa-2x"></i>&nbsp;
+                      <i class="fab fa-google fa-2x"></i>&nbsp;
+                      <i class="fab fa-linkedin fa-2x"></i>&nbsp;
+                      <i class="fab fa-twitter fa-2x"></i>&nbsp;<br>
+                      <h6>By Sliding to Continue with or Create My account , <br> I agree to<a href="index.html">alibaba.com Policy</a> </h6>
+                        <h2 style="color: black;">My Alibaba</h2>
+                        _____________________________________
+                         Manage RFQ <br>
+                         Orders <br>
+                         Favorites <br>
+                         Accounts <br>
+                         ___________ <br>
+                         Submit RFQ
+                    </div>
+                    <i class="far fa-user fa-2x ">&nbsp</i>Sign In <br>join free
+                </div>
+            </a>
+
+            <a href="message.html">
+                <div id="asign">
+                    <div id="msghover">
+                        <h3>Unread message reminder</h3><br>
+                        <p>we will Remind you  here when you there is<br> new message. please login to view </p><br>
+                        <button>Sign In</button><br>
+                        New User? Please  <a href="signup.html">register</a>& Start your Business today!
+                    </div>
+                    <i class="far fa-envelope fa-2x"></i>
+                </div>
+            </a>
+            <a href="orders.html">
+                <div id="asign">
+                    <div id="orderhover">
+                        <h3>Trade Assurance</h3>
+                        <p>Protect your alibaba.com orders</p><br>
+                        <p>Trade Assurance Ensures:</p><br>
+                        <p>Product Quality</p><br>
+                        <button>Order With Trade Assurance </button>
+                    </div>
+                    <i class="fab fa-jedi-order fa-2x"></i>
+            </a>
+        </div>
+        <a href="cart.html">
+            <div id="asign">
+                <i class="fas fa-shopping-cart fa-2x"></i>
+            </div>
+        </a>
+    </div>
+    </div>
+    <div id="secnav">
+        <div id="left">
+            <a href="/#">
+                <div id="left_first">
+                    Categories
+                    <!-- </div> -->
+                    <div id="menu">
+                        <ul>
+                            <li><a href="#">Machinery / Vehicles & Accessories </a>
+                                <ul>
+                                    <li><a href="#">Agricultural Machinery & Equipment</a></li>
+                                    <li><a href="#">Apparel & Textile Machinery</a></li>
+                                    <li><a href="#">Building Material Machinery</a></li>
+                                    <li><a href="#">Chemical & Pharamaceutical Machinery</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="./electronics.html">Consumer Electronics / Home Appliances </a>
+                                <ul>
+                                    <li><a href="#">Blockchain Miners</a></li>
+                                    <li><a href="#">Camera,Photo & Accessories</a></li>
+                                    <li><a href="#">Chargers,Batteries & Power Supplies</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Apparel / Fashion Accessories /Timepieces,Jewelry,Eyewear</a>
+                                <ul>
+                                    <li><a href="#">Men's Clothing</a></li>
+                                    <li><a href="#">women's Clothing</a></li>
+                                    <li><a href="#">Sportswear</a></li>
+                                    <li><a href="#">Garments & Processing Accessories</a></li>
+                                    <li><a href="#">Wedding Apparel & Accessories</a></li>
+                                    <li><a href="#">Made In Italy</a></li>
+                                    <li><a href="#">View More</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Lights & Lighting / Construction & Real Estate</a>
+                                <ul>
+                                    <li><a href="#">Commercial & industrial Lighting</a></li>
+                                    <li><a href="#">healthcare Lighting</a></li>
+                                    <li><a href="#">Holiday Lighting</a></li>
+                                    <li><a href="#">Indoor Lighting</a></li>
+                                    <li><a href="#">Landscape Lighting</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Home & Garden / Furniture</a>
+                                <ul>
+                                    <li><a href="#">Kitchen & Tabletop</a></li>
+                                    <li><a href="#">Garden supplies</a></li>
+                                    <li><a href="#">Pet Products</a></li>
+                                    <li><a href="#">Home Storage & Organisation</a></li>
+                                    <li><a href="#">Household & Organisation</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Fabric & Textiles Raw Material / Home Textiles</a>
+                                <ul>
+                                    <li><a href="#">Down & Feather</a></li>
+                                    <li><a href="#">Fabric</a></li>
+                                    <li><a href="#">Fiber</a></li>
+                                    <li><a href="#">Fur</a></li>
+                                    <li><a href="#">Leather</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Beauty & Personal Care / Health & Medical</a>
+                                <ul>
+                                    <li><a href="#">Baby Care</a></li>
+                                    <li><a href="#">Bath Supplies</a></li>
+                                    <li><a href="#">Beauty Equipment</a></li>
+                                    <li><a href="#">Body Art</a></li>
+                                    <li><a href="#"> False Eye slashes </a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Packaging & Printing / Office & School Supplies</a>
+                                <ul>
+                                    <li><a href="#">Sub Menu 1</a></li>
+                                    <li><a href="#">Sub Menu 2</a></li>
+                                    <li><a href="#">Sub Menu 3</a></li>
+                                    <li><a href="#">Sub Menu 4</a></li>
+                                    <li><a href="#">Sub Menu 5</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Electrical Equipment & Supplies</a>
+                                <ul>
+                                    <li><a href="#">Sub Menu 1</a></li>
+                                    <li><a href="#">Sub Menu 2</a></li>
+                                    <li><a href="#">Sub Menu 3</a></li>
+                                    <li><a href="#">Sub Menu 4</a></li>
+                                    <li><a href="#">Sub Menu 5</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Tools & Hardware / Security & Protection / Fabrication Service</a>
+                                <ul>
+                                    <li><a href="#">Sub Menu 1</a></li>
+                                    <li><a href="#">Sub Menu 2</a></li>
+                                    <li><a href="#">Sub Menu 3</a></li>
+                                    <li><a href="#">Sub Menu 4</a></li>
+                                    <li><a href="#">Sub Menu 5</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </a>
+
+            <a href="">
+                <div id="lefta">
+                    Ready to Ship
+                </div>
+            </a>
+            <a href="">
+                <div id="lefta">
+                    Personal Protective
+                </div>
+            </a>
+            <a href="">
+                <div class="drop">
+                    Buyer central
+                    <div class="sub_drop">
+                        <span style="display: flex;">
+                            <div>
+                                <p>Blog </p>
+                                <p>Trade Assurance</p>
+                                <p>Letter Of Credit</p>
+                            </div>
+                            <div>
+                                <p>&nbsp;&nbsp; Submit RFQ</p>
+                                <p>&nbsp;&nbsp;Suppliers By Regions</p>
+                            </div>
+                        </span>
+                    </div>
+
+                </div>
+            </a>
+            <a href="">
+                <div class="drop">
+                    sell on Alibaba
+                    <div class="sub_drop">
+                        <span style="display: flex;">
+                            <div>
+                                <p>For Global Seller </p>
+                                <p>For Chinese Seller</p>
+                                <p>Partner Program</p>
+                            </div>
+                            <div>
+                                <p>&nbsp;&nbsp; Submit RFQ</p>
+                                <p>&nbsp;&nbsp;Suppliers By Regions</p>
+                            </div>
+                        </span>
+                    </div>
+                </div>
+            </a>
+            <a href="">
+                <div class="drop">Help
+<div class="sub_drop">
+    <span style="display: flex;">
+        <div>
+            <p>for Buyers </p>
+            <p>For Suppliers</p>
+            <p>Submit a Dispute</p>
+            <p>Report IPR Infringement</p>
+            <p></p>
+        </div>
+    </span>
+</div>
+                </div>
+            </a>
+        </div>
+        <div id="right">
+            <div class="drop">Get the App</div>|
+            <div class="drop">English-INR</div>|
+            <div class="drop">Ship To: </div>
+        </div>
+    </div>`
+}
+
+export default navbar;
---
+++ script/carpart.js
@@ -0,0 +1,240 @@
+let carProduct = [
+    {
+        id : 1,
+        productTitle: '12v 5kw caravan car air diesel auto parking heater for ev rv',
+        price: 6935.40,
+        qty : 1,
+        productImg: [
+            "https://sc04.alicdn.com/kf/H5413e7cafb99486594dd8aae4081a342X.png",
+            "https://sc04.alicdn.com/kf/HTB192NiXqL7gK0jSZFBq6xZZpXaP.jpg",
+            "https://sc04.alicdn.com/kf/H2095907730c24f4c9c42e824c68366fbI.jpg"
+        ]
+    },
+    {
+        id : 2,
+        productTitle: '8inch tablet pc android rugged industrial pc car screen for vahivle traking heavy duty truk with gps',
+        price: 2697100,
+        qty : 1,
+        productImg: [
+            "https://sc04.alicdn.com/kf/H7c99959d26f94e66a4e6d0560ccce2390.jpg",
+            "https://sc04.alicdn.com/kf/H466cf5bede984c838ef8ad898662447cY.jpg",
+            "https://sc04.alicdn.com/kf/Ha6b2e0ee3c3d4addb53545de65540c4eb.jpg"
+        ]
+    },
+    {
+        id : 3,
+        productTitle: 'FM Transmitter Audio Mp3 3.1A Quick Bluetooth Kit Car',
+        price: 153.35,
+        qty : 1,
+        productImg: [
+            "https://sc04.alicdn.com/kf/He1e5eedda0644a54a2d1e244c1250246N.jpg",
+            "https://sc04.alicdn.com/kf/H92aac69e86f0420fab27f08f622180dcG.jpg",
+            "https://sc04.alicdn.com/kf/H79b48782bad74590bb69353c81954b29w.jpg"
+        ]
+    },
+    {
+        id : 4,
+        productTitle: 'Lithium 12v Car Battery 80Ah Car Battery 6-qw-80 90ah Auto',
+        price: 3542.38,
+        qty : 1,
+        productImg: [
+            "https://sc04.alicdn.com/kf/Hcede0d1effa74aa387ca2ec5c1aef9d0i.jpg_50x50.jpg",
+            "https://sc04.alicdn.com/kf/He9b8eef0a10b41dd99e978ca79a43f553.jpg",
+            "https://sc04.alicdn.com/kf/H5d5f82d1a01c4b14b2a50f161560bfffo.jpg"
+        ]
+    },
+    {
+        id : 5,
+        productTitle: '171 pcs Hot sale New Repair Tools Box Set Automobile Repair Tools Kit',
+        price: 3066.99,
+        qty : 1,
+        productImg: [
+            "https://sc04.alicdn.com/kf/H535bf24afb044aa79d7493a1bdabacaaC.jpg_50x50.jpg",
+            "https://sc04.alicdn.com/kf/H445030017e8745629727f18d89efa1eaI.jpg",
+            "https://sc04.alicdn.com/kf/He14b0532f11449ad9f388f0a620c1366W.jpg"
+        ]
+    },
+    {
+        id : 6,
+        productTitle: 'Super PDR Car Dent Repair Puller Suction Cup Bodywork Panel Sucker Remover Tool For dent repair auto tools',
+        price: 142.39,
+        qty : 1,
+        productImg: [
+            "https://sc04.alicdn.com/kf/HTB1Vh7seQ5E3KVjSZFCq6zuzXXaT.jpg",
+            "https://sc04.alicdn.com/kf/Hcbd67c1d9d914443a2d15216a190aadaq.jpg",
+            "https://sc04.alicdn.com/kf/Hd3793226a45744c3a1c0cbf04ce62ba3Z.jpg"
+        ]
+    },
+    {
+        id : 7,
+        productTitle: 'Water Pump fit for Prius 2004-2009,G9020-478031',
+        price: 1695.32,
+        qty : 1,
+        productImg: [
+            "https://sc04.alicdn.com/kf/H67e7446624104cf39f52c0d2689056d3y.png",
+            "https://sc04.alicdn.com/kf/H636698c6db464acaaf2fa92f4cd461095.png",
+            "https://sc04.alicdn.com/kf/Hf71249410a984fa381c1bc49cbced198j.png"
+        ]
+    },
+    {
+        id : 8,
+        productTitle: '12V/24V Universe Dual USB %v 4.8A Device Charger 2USB Ports For Car Marine Bus Atv Utv Boat Compatible With Iphones ,Android',
+        price: 322.12,
+        qty : 1,
+        productImg: [
+            "https://sc04.alicdn.com/kf/Hce92c0da20a647ffbc9f94a7c4a247abh.jpg",
+            "https://sc04.alicdn.com/kf/H7faafb3f1c05412baf83e1a2bd4740548.jpg",
+            "https://sc04.alicdn.com/kf/H8383238e16d54c858b02875311d4e7b0a.jpg"
+        ]
+    },
+    {
+        id : 9,
+        productTitle: 'Car windshield Crack Repair Tool Glass Scratch Restore Resin DIY WindScreen Repair Kit Tool With Blade And Strips',
+        price: 122.53,
+        qty : 1,
+        productImg: [
+            "https://sc04.alicdn.com/kf/Hfd40fe42613c40149a4e9b7337e884b1B.jpg",
+            "https://sc04.alicdn.com/kf/H5aa2391d2cf94cae8fd0192b76d67035g.jpg",
+            "https://sc04.alicdn.com/kf/H08a66799baba40f9b85cee3c3691919au.jpg"
+        ]
+    },
+    {
+        id : 10,
+        productTitle: 'Hanging Tree provide Long lasting Scent Fr Car Air Freshener little trees',
+        price: 62.56,
+        qty : 1,
+        productImg: [
+            "https://sc04.alicdn.com/kf/H901b5f711c7b463eaa1f7691b53e99cdH.jpg",
+            "https://sc04.alicdn.com/kf/H18aa5e23031344e68415398fae126a18Q.jpg",
+            "https://sc04.alicdn.com/kf/H411b8f3efbab46ebb5574429dc615ddau.jpg"
+        ]
+    },
+    {
+        id : 11,
+        productTitle: 'Whole sell car Dash Camera Full HD Car Black Box For Car DVR Camera Factory Dual Lens Dashcam With WIFI Function DAsh Cam',
+        price: 1695.37,
+        qty : 1,
+        productImg: [
+            "https://sc04.alicdn.com/kf/H10eaf657af93433a849bd12c0692f97ay.jpg",
+            "https://sc04.alicdn.com/kf/H15bcdbb69e2a44a0a80003d0b4f3916eX.jpg",
+            "https://sc04.alicdn.com/kf/Hbe9a19165f1b4582bea4422d0b456f0f3.jpg"   ]
+    },
+    {
+        id :12 ,
+        productTitle: '12 V 7 480*800 IPS MP4 MP5tv android car monitor',
+        price: 2157.68,
+        qty : 1,
+        productImg: [
+            "https://sc04.alicdn.com/kf/Hc184b8e144bb48e3b1fa43ee89f701caC.jpg",
+            "https://sc04.alicdn.com/kf/Hb429c6bab7ce4eb0b1312ec964099035U.jpg",
+            "https://sc04.alicdn.com/kf/H5e475083992640c9841cf5476a6e9560Q.jpg"   ]
+    },
+    {
+        id : 13,
+        productTitle: 'LED HEdlight Auto Headlamp Bulbs H1 H H7 Car led Head fog head kights lamps',
+        price: 84.77,
+        qty : 1,
+        productImg: [
+            "https://sc04.alicdn.com/kf/H28e0dd8fbeca49019a34d16f94a75bcfn.jpg_50x50.jpg",
+            "https://sc04.alicdn.com/kf/H5c57bda362f04969906e215a320ed8a8C.jpg",
+            "https://sc04.alicdn.com/kf/H3f7823c3fb9642a69d227e5bed5d7260o.jpg"   ]
+    },
+    {
+        id : 14,
+        productTitle: 'GapV Factory Price Cat headlaps Car auto LEd Headlight for Toyota Lexus camry corolla RaV4 Vios For Japanese Car',
+        price: 7706.00,
+        qty : 4,
+        productImg: [
+            "https://sc04.alicdn.com/kf/H3fd0455f361442e9b10a6782dcd3bb9a5.jpg",
+            "https://sc04.alicdn.com/kf/H399cf543c0af4a3e9b16f3bca7681120D.jpg",
+            "https://sc04.alicdn.com/kf/H5c00e5b144f04c239c836fbd6423508av.jpg"   ]
+    },
+    {
+        id :15 ,
+        productTitle: 'Cordless Portable High Pressure Electric Car asher Gun With 24V Recharchable Battery Power car Wash Foam gun',
+        price: 2003.56,
+        qty : 2,
+        productImg: [
+            "https://sc04.alicdn.com/kf/H1a002ff8445347bdbe7594c493375917H.jpg",
+            "https://sc04.alicdn.com/kf/H57cbd02d7d7148159cad71baa2801a90P.jpg",
+            "https://sc04.alicdn.com/kf/H7144e745e62241fc8621e146b399bf25i.jpg_50x50.jpg"   ]
+    },
+    {
+        id : 16,
+        productTitle: '12V/24V 25-150 ah Car Portable Battery Charge Jump Starter ',
+        price: 1386.52,
+        qty : 1,
+        productImg: [
+            "https://sc04.alicdn.com/kf/H5987d9fbd2bd4101b57cbda33b5fbd5fH.jpg",
+            "https://sc04.alicdn.com/kf/Hd6240ef6378a4ba88df99bfb454df449d.jpg",
+            "https://sc04.alicdn.com/kf/Hd6240ef6378a4ba88df99bfb454df449d.jpg"   ]
+    },
+    {
+        id : 17,
+        productTitle: 'flip down 7 inch lcd screen car mp5 monitor',
+        price: 1048.25,
+        qty : 1,
+        productImg: [
+            "https://sc04.alicdn.com/kf/H313cd05966554145bd576022b666bb18k.jpg",
+            "https://sc04.alicdn.com/kf/Hbdb34cde0f1b4d0e90752c2d6d839d01A.jpg",
+            "https://sc04.alicdn.com/kf/H307a2807e8404f86989cbdead8f10887T.jpg"   ]
+    },
+    {
+        id : 18,
+        productTitle: 'GJF Auto spatre parts Front CV Axle Drive Shaft Inner Outer CV Joint For Toyota Nissan Honda Hyundai Ford Volvo',
+        price:649.00 ,
+        qty : 1,
+        productImg: [
+            "https://sc04.alicdn.com/kf/H40efaf67b1f44ca3a82a730edaf4ca4aK.jpg",
+            "https://sc04.alicdn.com/kf/Had69516af69c447d95466e118f35e383H.jpg",
+            "https://sc04.alicdn.com/kf/H0e6c40a5021b43b69d164790c0ffc581u.jpg"   ]
+    },
+    {
+        id :19 ,
+        productTitle: 'Univeral Sound Simulator Car Turbo Sound Whistle VEhicle Reflit Device Exhaust Pipe Turbo Sound Whidltle Car turbo Muffler ',
+        price: 50.86,
+        qty : 1,
+        productImg: [
+            "https://sc04.alicdn.com/kf/He892b05f44764de1a2f0db5319b2873ew.jpg",
+            "https://sc04.alicdn.com/kf/H1780024833b84cfea86e6aaa26520b05a.jpg",
+            "https://sc04.alicdn.com/kf/H8bf4057749bb4b4bbf56f65c1016b62eh.jpg_50x50.jpg"   ]
+    },
+    
+    {
+        id : 20,
+        productTitle: 'Multifunctional Effervescent spray Glass Car Windshield cleaner Tablet',
+        price: 1.55,
+        qty : 1,
+        productImg: [
+            "https://sc04.alicdn.com/kf/H7fd7604167e643c8ade4206606d553cbV.png",
+            "https://sc04.alicdn.com/kf/H7afa1a8a925442c78cc4f16b41ad4df2p.png",
+            "https://sc04.alicdn.com/kf/H5de17d66208140c59f7bd77031aa6181N.jpg"
+        ]
+    }
+];
+
+displayProducts(carProduct);
+
+function displayProducts(data){
+    data.forEach((element)=>{
+        let { id, productTitle,price, productImg } = element;
+        
+        let card = document.createElement("div");
+        card.onclick = ()=>{
+            localStorage.setItem("openedProduct",JSON.stringify(element));
+            window.location.href = "./openproduct.html";
+        };
+
+        let product_img = document.createElement("img");
+        product_img.src = productImg[0];
+
+        let product_title = document.createElement("p");
+        product_title.textContent = productTitle;
+
+        let product_price = document.createElement("p");
+        product_price.textContent = "₹ "+price;
+
+        card.append(product_img,product_title,product_price);
+        document.querySelector("#Car").append(card);
+    });
+}
